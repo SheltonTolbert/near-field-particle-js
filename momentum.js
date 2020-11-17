713cf0083c1ea92203f929a9f6b10daf5482c357
Shelton_T <60357311+SheltonTolbert@users.noreply.github.com>
2020-11-17 05:48:24 -0600

    Update init particle spawn loc
    Als moved all parameter variables to the top of the script

--- momentum.js
+++ momentum.js
@@ -2,7 +2,7 @@ var boundary;
 var canvas; 
 var nodes = []; 
 var threshold; 
-var numNodes; 
+var numNodes = 100; 
 var speed = 1;
 
 class Vector
@@ -101,15 +101,15 @@ function drawLine(context, v1, v2, alpha)
 function createNode()
 {
       // Get a random line to spawn a new Node
-      index = Math.floor(Math.random() * 4)
-      randLine = boundary[index]
-      startingPoint = getRandomPoint(randLine)
+      index = Math.floor(Math.random() * 4);
+      randLine = boundary[index];
+      startingPoint = new Vector(Math.random() * canvas.width, Math.random() * canvas.height );
       // Choose a line that is not the origin line to choose a target point
       randLine2 = randLine;
       while (randLine2 == randLine)
       {
-          index = Math.floor(Math.random() * 4)
-          randLine2 = boundary[index]
+          index = Math.floor(Math.random() * 4);
+          randLine2 = boundary[index];
       }
       // Set a target point for the node
       targetPoint = getRandomPoint(randLine2);
@@ -118,53 +118,10 @@ function createNode()
 
 
 
-
-
-
-
-
-
-
-function init()
+function nearFieldNodes(canvas)
 {
-    window.requestAnimationFrame(draw);
-    // Create boundry shape
-    canvas = document.getElementById('canvas');
-    canvas.width = document.body.clientWidth; //document.width is obsolete
-    canvas.height = document.body.clientHeight; //document.height is obsolete
-    
-    
-    
-    
-    boundary = [
-        new Line(0, 0, canvas.width, 0),
-        new Line(canvas.width, 0, canvas.width, canvas.height),
-        new Line(canvas.width, canvas.height, 0, canvas.height),
-        new Line(0, canvas.height, 0, 0), 
-    ];
-    // set threshold for lines
-    threshold = 100;
-
-    // Create a new node
-    numNodes = 200;
-    for(let i = 0; i < numNodes; i++)
-    {
-        nodes.push(createNode());
-    }
-}
-
-
-
-function draw()
-{
-    canvas = document.getElementById('canvas');
-    var context = canvas.getContext("2d");
-    
-    context.globalCompositeOperation = 'destination-over';
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-
-    // Destroy nodes that exit the boundaries
+    context = context = canvas.getContext("2d");
+    // Destroy nodes that exit the boundaries by seperating them from the nodes in bounds
     var nodesInBounds = []
     for(let i = 0; i < nodes.length; i++)
     {
@@ -190,7 +147,8 @@ function draw()
             else
             {
                 let distance = getDistance(nodes[i], nodes[j]);
-                //new_value = (old_value - old_bottom) / (old_top - old_bottom) * (new_top - new_bottom) + new_bottom;
+                // use a mapping function to convert the distance between nodes to a value between 1 and zero
+                // new_value = (old_value - old_bottom) / (old_top - old_bottom) * (new_top - new_bottom) + new_bottom;
                 if (distance < threshold)
                 {   
                     // Map alpha 
@@ -203,7 +161,6 @@ function draw()
             }
         }
     }
-    // Draw nodes
     // move Nodes
     for(let i = 0; i < nodes.length; i++)
     {
@@ -211,7 +168,6 @@ function draw()
         nodes[i].move();        
         
     }
-    
     // Repopulate the nodes array
     nodes = nodesInBounds
     if (nodes.length < numNodes)
@@ -223,6 +179,47 @@ function draw()
             nodes.push(createNode());
         }
     }
+}
+
+
+
+
+
+
+function init()
+{
+    window.requestAnimationFrame(draw);
+    // Create boundry shape
+    canvas = document.getElementById('canvas');
+    canvas.width = document.body.clientWidth; //document.width is obsolete
+    canvas.height = document.body.clientHeight; //document.height is obsolete
+    boundary = [
+        new Line(0, 0, canvas.width, 0),
+        new Line(canvas.width, 0, canvas.width, canvas.height),
+        new Line(canvas.width, canvas.height, 0, canvas.height),
+        new Line(0, canvas.height, 0, 0), 
+    ];
+    // set threshold for lines
+    threshold = 100;
+    // Create a new node
+    for(let i = 0; i < numNodes; i++)
+    {
+        nodes.push(createNode());
+    }
+}
+
+
+
+function draw()
+{
+    canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+    
+    context.globalCompositeOperation = 'destination-over';
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    nearFieldNodes(canvas);
+
     
     window.requestAnimationFrame(draw);
 }
